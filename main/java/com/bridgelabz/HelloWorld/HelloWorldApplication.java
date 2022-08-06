9adff3d443e262e8b8e2c762cb7c60b32307192c
harish15x <hyadav15x@gmail.com>
2022-08-06 16:26:13 +0530

    Added: day 2 assignment

---
+++ main/java/com/bridgelabz/HelloWorld/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.HelloWorld;
+
+import com.bridgelabz.HelloWorld.controller.HelloWorldController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+
+	}
+
+}
